b4eade8f14700e1c61a9a3dca7694d3fc4ec3aaa
Mr.P_o_s_T <popovich601@gmail.com>
2018-04-14 16:56:10 +0300

    Ex.11: Operator class described.

--- Ex.11/Ex.11/ITerm.h
+++ Ex.11/Ex.11/ITerm.h
@@ -4,9 +4,8 @@ class ITerm {
 public:
 	virtual ~ITerm() = 0;
 
-	virtual void addSubterm(ITerm &t) = 0;
-	virtual void deleteSubTerm(bool first) = 0;
+	virtual void addSubterm(ITerm *t) = 0;
+	virtual void deleteSubterm(size_t index) = 0;
+	virtual void deleteSubterms(size_t indexFrom, size_t indexTo) = 0;
 	virtual float getValue() = 0;
 };
-
-ITerm::~ITerm() { }
--- Ex.11/Ex.11/Operator.h
+++ Ex.11/Ex.11/Operator.h
@@ -1,19 +1,32 @@
 #pragma once
 #include "ITerm.h"
+#include <vector>
+#include <exception>
 
+template <size_t NArity>
 class Operator : public ITerm {
 public:
-	Operator();
-	~Operator();
+	Operator() = default;
+
+	void addSubterm(ITerm *t) {
+		operands.push_back(t);
+	}
+
+	void deleteSubterm(size_t index) {
+		delete operands[index];
+		operands.erase(operands.begin() + index);
+	}
+
+	void deleteSubterms(size_t indexFrom = 0, size_t indexTo = NArity - 1) {
+		for (size_t i = indexFrom; i <= indexTo; i++) delete operands[i];
+		operands.erase(operands.begin() + indexFrom, operands.begin() + indexTo);
+	}
+
+	virtual float getValue() = 0;
+
+	virtual ~Operator() {
+		for (size_t i = 0; i < operands.size(); i++) delete operands[i];
+	}
 private:
-
+	std::vector<ITerm *> operands;
 };
-
-Operator::Operator()
-{
-}
-
-
-Operator::~Operator()
-{
-}
