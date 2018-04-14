25dcf23649ca97525643674051846b9a349a006f
Mr.P_o_s_T <popovich601@gmail.com>
2018-04-14 16:53:15 +0300

    Ex.11: Value class description changed: ~Value() {} -> ~Value() = default

--- Ex.11/Ex.11/Value.h
+++ Ex.11/Ex.11/Value.h
@@ -4,8 +4,9 @@
 class Value : public ITerm {
 public:
 	Value(float value) { this->value = value; }
+	~Value() = default;
+
 	float getValue() { return value; }
-	~Value() { }
 private:
 	float value;
 };
