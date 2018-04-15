854252dfd3b2c751ee67ac67d95d8ac713e8d393
Mr.P_o_s_T <popovich601@gmail.com>
2018-04-15 17:02:28 +0300

    Ex.11: Adding more sequrity to Operator class

--- Ex.11/Ex.11/Operator.h
+++ Ex.11/Ex.11/Operator.h
@@ -8,17 +8,22 @@ public:
 	Operator() = default;
 
 	void addSubterm(ITerm *t) {
+		if (operands.size() >= NArity) throw std::exception("All operands are already added...");
 		operands.push_back(t);
 	}
 
 	void deleteSubterm(size_t index) {
+		if (index >= NArity) throw std::exception("Operand does not exist...");
 		delete operands[index];
 		operands.erase(operands.begin() + index);
+		
 	}
 
 	void deleteSubterms(size_t indexFrom = 0, size_t indexTo = NArity - 1) {
+		if ((indexFrom >= NArity) || (indexTo >= NArity)) throw std::exception("Operands does not exist...");
 		for (size_t i = indexFrom; i <= indexTo; i++) delete operands[i];
 		operands.erase(operands.begin() + indexFrom, operands.begin() + indexTo);
+		if (indexTo == NArity - 1) operands.pop_back();
 	}
 
 	virtual float getValue() = 0;
