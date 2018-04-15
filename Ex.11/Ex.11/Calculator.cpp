09b4f46058c66605583b6d917fc78a3066210b76
Mr.P_o_s_T <popovich601@gmail.com>
2018-04-15 16:27:25 +0300

    Ex.11: Calculator class - parser desctrption

--- Ex.11/Ex.11/Calculator.cpp
+++ Ex.11/Ex.11/Calculator.cpp
@@ -1,20 +1,76 @@
 #include "Calculator.h"
 
-Calculator::Calculator() {
+ITerm * Calculator::buildSubtree(std::string &expression) {
 
+	return nullptr;
 }
 
-Calculator::~Calculator() {
-
-}
-
-float Calculator::Parser9000(const std::string &expression) {
-	ITerm *rootTerm;
-	auto iter = expression;
-
-	while (true) {
-
+std::string Calculator::getSummand(std::string &expression) {
+	std::string res;
+	bool inBrackets = false, founded = false;
+	for (size_t i = 0; (i < expression.size()) && (!founded); i++) {
+		switch (expression[i]) {
+		case '+': 
+		case '-':
+			if (!inBrackets) {
+				founded = true;
+				break;
+			}
+			res += expression[i];
+			break;
+		case '(': inBrackets = true;
+			res += expression[i];
+			break;
+		case ')': inBrackets = false;
+			res += expression[i];
+			break;
+		default:
+			res += expression[i];
+			break;
+		}
 	}
+	res = expression.substr(0, expression.size() - res.size());
+	return res;
+}
 
-	return rootTerm->getValue();
+std::string Calculator::getFactor(std::string & expression) {
+	std::string res;
+	bool inBrackets = false, founded = false;
+	for (size_t i = 0; (i < expression.size()) && (!founded); i++) {
+		switch (expression[i]) {
+		case '*':
+		case '/':
+			if (!inBrackets) {
+				founded = true;
+				break;
+			}
+			res += expression[i];
+			break;
+		case '(': inBrackets = true;
+			res += expression[i];
+			break;
+		case ')': inBrackets = false;
+			res += expression[i];
+			break;
+		default:
+			res += expression[i];
+			break;
+		}
+	}
+	res = expression.substr(0, expression.size() - res.size());
+	return res;
+}
+
+float Calculator::getNumber(std::string &expression) {
+	float res = 0.0f;
+	size_t separator = 0;
+	for (size_t i = 0; i < expression.size(); i++) {
+		if ((expression[i] != '.') || (expression[i] != ',')) {
+			res *= 10.0f;
+			res += expression[i] - 48;
+		}
+		else separator = expression.size() - i - 1;
+	}
+	for ( ; separator > 0; separator--) res /= 10.0f;
+	return res;
 }
--- Ex.11/Ex.11/Calculator.h
+++ Ex.11/Ex.11/Calculator.h
@@ -5,9 +5,19 @@
 
 class Calculator {
 public:
-	Calculator();
-	~Calculator();
+	Calculator() = default;
+	~Calculator() = default;
 
-	float Parser9000(const std::string &expression);
+	float Parser9000(std::string expression) {
+		ITerm *tmp = buildSubtree(expression);
+		float res = tmp->getValue();
+		delete tmp;
+	}
+private:
+	ITerm *buildSubtree(std::string &expression);
+
+	std::string getSummand(std::string &expression);
+	std::string getFactor(std::string &expression);
+
+	float getNumber(std::string &expression);
 };
-
