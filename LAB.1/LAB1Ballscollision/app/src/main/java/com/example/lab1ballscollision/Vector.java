d9f33b2e6baab061e0b2bfca445f84402e2d42cc
Mr.P_o_s_T <popovich601@gmail.com>
2018-04-18 17:34:28 +0300

    Project created...

---
+++ LAB.1/LAB1Ballscollision/app/src/main/java/com/example/lab1ballscollision/Vector.java
@@ -0,0 +1,11 @@
+package com.example.lab1ballscollision;
+
+public class Vector {
+    public static Vector nullVector;
+
+    public double x, y;
+    
+    Vector(float nX, float nY) {
+        x = nX; y = nY;
+    }
+}
