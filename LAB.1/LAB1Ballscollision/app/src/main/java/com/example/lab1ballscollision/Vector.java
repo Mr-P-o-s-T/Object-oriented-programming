8faac24df175abd9cc9fe782ab9830958ef195e7
Mr.P_o_s_T <popovich601@gmail.com>
2018-04-19 20:50:06 +0300

    Vector java class implemented...

--- LAB.1/LAB1Ballscollision/app/src/main/java/com/example/lab1ballscollision/Vector.java
+++ LAB.1/LAB1Ballscollision/app/src/main/java/com/example/lab1ballscollision/Vector.java
@@ -1,11 +1,55 @@
 package com.example.lab1ballscollision;
 
+import java.math.*;
+
 public class Vector {
     public static Vector nullVector;
 
+    static {
+        nullVector = new Vector(0.0,0.0);
+    }
+
     public double x, y;
-    
-    Vector(float nX, float nY) {
+
+    public Vector(double nX, double nY) {
         x = nX; y = nY;
     }
+
+    public Vector () {
+        this(nullVector);
+    }
+
+    public Vector(Vector origin) {
+        x = origin.x;
+        y = origin.y;
+    }
+
+    public double length() {
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public Vector add(Vector b) {
+        return new Vector(x + b.x, y + b.y);
+    }
+
+    public Vector subtract(Vector b) {
+        return new Vector(x - b.x, y - b.y);
+    }
+
+    public Vector multOnScalar(double scalar) {
+        return new Vector(x * scalar,y * scalar);
+    }
+
+    public double scalarCompos(Vector b) {
+        return x * b.x + y * b.y;
+    }
+
+    public double vectorCompos(Vector b) {
+        return x * b.y - y * b.x;
+    }
+
+    public double alpha(Vector b) {
+        return Math.asin(vectorCompos(b) / (length() * b.length()));
+    }
+
 }
