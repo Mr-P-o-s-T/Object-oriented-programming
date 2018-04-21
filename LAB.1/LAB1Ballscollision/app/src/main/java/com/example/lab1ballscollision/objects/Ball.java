e2c2d58173611fde186674091adae377455d7278
Mr.P_o_s_T <popovich601@gmail.com>
2018-04-21 15:59:49 +0300

    Ball java abstract class described

--- LAB.1/LAB1Ballscollision/app/src/main/java/com/example/lab1ballscollision/objects/Ball.java
+++ LAB.1/LAB1Ballscollision/app/src/main/java/com/example/lab1ballscollision/objects/Ball.java
@@ -1,26 +1,39 @@
 package com.example.lab1ballscollision.objects;
 
-public class Ball {
+public abstract class Ball {
     protected Vector position, impulse;
-    protected float radius;
+    protected float mass;
 
-    Ball(double x, double y, float r) {
+    Ball(double x, double y, float m) {
         position = new Vector(x, y);
         impulse = new Vector();
-        radius = r;
+        mass = m;
     }
+
     Ball(Ball origin) {
         position = new Vector(origin.position);
         impulse = new Vector(origin.impulse);
-        radius = origin.radius;
+        mass = origin.mass;
     }
 
-    void changePosition(double deltaT) {
-        position = position.add(impulse.multOnScalar(deltaT).multOnScalar(1 / radius));
+    abstract double getRadius();
+
+    void positionChanging(double deltaT) {
+        position = position.add(impulse.multOnScalar(deltaT).multOnScalar(1 / mass));
     }
 
     boolean areCollided(Ball secondBall) {
         Vector delta = position.subtract(secondBall.position);
-        return delta.length() < radius + secondBall.radius;
+        return delta.length() < getRadius() + secondBall.getRadius();
+    }
+
+    void impulseChanging(double speedX, double speedY) {
+        impulse.x = speedX * mass;
+        impulse.y = speedY * mass;
+    }
+
+    void relativeImpluseChanging(double deltaSpeedX, double deltaSpeedY) {
+        impulse.x += deltaSpeedX * mass;
+        impulse.y += deltaSpeedY * mass;
     }
 }
