4714154f780717bc4a16dab119e06fa09180e472
Mr.P_o_s_T <popovich601@gmail.com>
2018-04-21 18:27:12 +0300

    Making Vector more 3D...

--- LAB.1/LAB1Ballscollision/app/src/main/java/com/example/lab1ballscollision/objects/Vector.java
+++ LAB.1/LAB1Ballscollision/app/src/main/java/com/example/lab1ballscollision/objects/Vector.java
@@ -4,13 +4,17 @@ class Vector {
     public static Vector nullVector;
 
     static {
-        nullVector = new Vector(0.0,0.0);
+        nullVector = new Vector(0.0,0.0, 0.0);
     }
 
-    public double x, y;
+    public double x, y, z;
+
+    public Vector(double nX, double nY, double nZ) {
+        x = nX; y = nY; z = nZ;
+    }
 
     public Vector(double nX, double nY) {
-        x = nX; y = nY;
+        this(nX, nY, 0.0);
     }
 
     public Vector () {
@@ -20,34 +24,41 @@ class Vector {
     public Vector(Vector origin) {
         x = origin.x;
         y = origin.y;
+        z = origin.z;
     }
 
     public double length() {
-        return Math.sqrt(x * x + y * y);
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public Vector normalise() {
+        double len = length();
+        return new Vector(x / len, y / len, z / len);
     }
 
     public Vector add(Vector b) {
-        return new Vector(x + b.x, y + b.y);
+        return new Vector(x + b.x, y + b.y, z + b.z);
     }
 
     public Vector subtract(Vector b) {
-        return new Vector(x - b.x, y - b.y);
+        return new Vector(x - b.x, y - b.y, z - b.z);
     }
 
     public Vector multOnScalar(double scalar) {
-        return new Vector(x * scalar,y * scalar);
+        return new Vector(x * scalar,y * scalar, z * scalar);
     }
 
     public double scalarCompos(Vector b) {
-        return x * b.x + y * b.y;
+        return x * b.x + y * b.y + z * b.z;
     }
 
-    public double vectorCompos(Vector b) {
-        return x * b.y - y * b.x;
+    public Vector vectorCompos(Vector b) {
+        return new Vector(y * b.z - z * b.y, z * b.x
+                 - x * b.z,x * b.y - y * b.x);
     }
 
     public double alpha(Vector b) {
-        return Math.asin(vectorCompos(b) / (length() * b.length()));
+        return Math.asin(vectorCompos(b).length() / (length() * b.length()));
     }
 
 }
