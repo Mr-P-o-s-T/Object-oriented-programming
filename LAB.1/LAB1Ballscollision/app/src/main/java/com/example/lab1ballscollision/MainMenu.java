8cdc784043e14292c85a80b590f34591e0e9263c
Mr.P_o_s_T <popovich601@gmail.com>
2018-05-01 21:48:48 +0300

    Environment`s properties screed has been added...

--- LAB.1/LAB1Ballscollision/app/src/main/java/com/example/lab1ballscollision/MainMenu.java
+++ LAB.1/LAB1Ballscollision/app/src/main/java/com/example/lab1ballscollision/MainMenu.java
@@ -1,7 +1,10 @@
 package com.example.lab1ballscollision;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void onStartClick(View view) {
+        Intent intent = new Intent(MainMenu.this, PropertiesActivity.class);
+        startActivity(intent);
+    }
+
+    public void onExitClick(View view) {
+        finish();
+        Toast.makeText(this, "Already leaving?", Toast.LENGTH_SHORT).show();
     }
 }
