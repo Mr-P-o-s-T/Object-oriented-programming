970ac325a26b6d2445a25b3c4a6eb0023bcb441b
Mr.P_o_s_T <popovich601@gmail.com>
2018-06-02 22:03:05 +0300

    Adding of bucket-sorting as common part of sorting

---
+++ LAB.8/LAB.8 (Single threaded)/BucketSorter.cpp
@@ -0,0 +1,39 @@
+#define SORT_METHOD_SWITCHER_NUM 5
+#include "BucketSorter.h"
+#include "CombSorter.h"
+using namespace std;
+
+void BucketSorter::sortMethod() {
+	min = data->front(), max = min;
+	for (size_t i = 0; i < data->size(); i++) {
+		if ((*data)[i] < min) min = (*data)[i];
+		else if ((*data)[i] > max) max = (*data)[i];
+	}
+	if (min != max) {
+		max += 1;
+		Bucket *buckets = new Bucket[k];
+
+		for (size_t i = 0; i < data->size(); i++) {
+			buckets[getIndex((*data)[i])].AddData((*data)[i]);
+		}
+
+		for (size_t i = 0; i < k; i++) {
+			Sorter *sorter;
+			if (buckets[i].GiveData().size() <= SORT_METHOD_SWITCHER_NUM) sorter = new CombSorter();
+			else sorter = new BucketSorter(k);
+			sorter->Sort(buckets[i]);
+			delete sorter;
+		}
+
+		vector<int> res;
+		for (size_t i = 0; i < k; i++) {
+			size_t nI = buckets[i].GiveData().size();
+			for (size_t j = 0; j < nI; j++) res.push_back(buckets[i].GiveData()[j]);
+		}
+
+		data->clear();
+		swap(*data, res);
+
+		delete[] buckets;
+	}
+}
