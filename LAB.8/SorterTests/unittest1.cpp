e8296db15136f05147c0685d6c7fe2cb7df84901
Mr.P_o_s_T <popovich601@gmail.com>
2018-06-05 16:22:33 +0300

    Tests are added

---
+++ LAB.8/SorterTests/unittest1.cpp
@@ -0,0 +1,93 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../LAB.8 (Multi threaded)/BucketSorter.cpp"
+#include "../LAB.8 (Multi threaded)/CombSorter.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SorterTests {		
+	TEST_CLASS(CombSorterTests) {
+	public:
+		TEST_METHOD(PositiveArraySorting) {
+			std::vector<int> arrExample({ 8, 3 , 7 , 8 , 10, 15, 3, 1, 0 }), sortedExample({ 0, 1, 3, 3, 7, 8, 8, 10, 15 });
+			Bucket bckt(arrExample);
+			CombSorter srtr;
+			srtr.Sort(bckt);
+			std::vector<int> res = bckt.GiveData();
+			for (size_t i = 0; i < res.size(); i++) {
+				if (sortedExample[i] != res[i]) Assert::Fail();
+			}
+		}
+
+		TEST_METHOD(CommonArraySorting) {
+			std::vector<int> arrExample({ 8, -3 , 7 , -8 , 10, -15, 3, -1, 0 }), sortedExample({ -15, -8, -3, -1, 0, 3, 7, 8, 10 });
+			Bucket bckt(arrExample);
+			CombSorter srtr;
+			srtr.Sort(bckt);
+			std::vector<int> res = bckt.GiveData();
+			for (size_t i = 0; i < res.size(); i++) {
+				if (sortedExample[i] != res[i]) Assert::Fail();
+			}
+		}
+
+		TEST_METHOD(OneElementArraySorting) {
+			std::vector<int> arrExample({ 8 }), sortedExample({ 8 });
+			Bucket bckt(arrExample);
+			CombSorter srtr;
+			srtr.Sort(bckt);
+			std::vector<int> res = bckt.GiveData();
+			for (size_t i = 0; i < res.size(); i++) {
+				if (sortedExample[i] != res[i]) Assert::Fail();
+			}
+		}
+
+		TEST_METHOD(EmptyArraySorting) {
+			std::vector<int> arrExample({ });
+			Bucket bckt(arrExample);
+			CombSorter srtr;
+			srtr.Sort(bckt);
+		}
+	};
+
+	TEST_CLASS(BucketSorterTests) {
+	public:
+		TEST_METHOD(PositiveArraySorting) {
+			std::vector<int> arrExample({ 8, 3 , 7 , 8 , 10, 15, 3, 1, 0 }), sortedExample({ 0, 1, 3, 3, 7, 8, 8, 10, 15 });
+			Bucket bckt(arrExample);
+			BucketSorter srtr(2);
+			srtr.Sort(bckt);
+			std::vector<int> res = bckt.GiveData();
+			for (size_t i = 0; i < res.size(); i++) {
+				if (sortedExample[i] != res[i]) Assert::Fail();
+			}
+		}
+
+		TEST_METHOD(CommonArraySorting) {
+			std::vector<int> arrExample({ 8, -3 , 7 , -8 , 10, -15, 3, -1, 0 }), sortedExample({ -15, -8, -3, -1, 0, 3, 7, 8, 10 });
+			Bucket bckt(arrExample);
+			BucketSorter srtr(2);
+			srtr.Sort(bckt);
+			std::vector<int> res = bckt.GiveData();
+			for (size_t i = 0; i < res.size(); i++) {
+				if (sortedExample[i] != res[i]) Assert::Fail();
+			}
+		}
+
+		TEST_METHOD(OneElementArraySorting) {
+			std::vector<int> arrExample({ 8 }), sortedExample({ 8 });
+			Bucket bckt(arrExample);
+			BucketSorter srtr(2);
+			srtr.Sort(bckt);
+			std::vector<int> res = bckt.GiveData();
+			for (size_t i = 0; i < res.size(); i++) {
+				if (sortedExample[i] != res[i]) Assert::Fail();
+			}
+		}
+
+		TEST_METHOD(EmptyArraySorting) {
+			std::vector<int> arrExample({ });
+			Bucket bckt(arrExample);
+			BucketSorter srtr(2);
+			srtr.Sort(bckt);
+		}
+	};
+}
