99c65b70b7930b35df84c1b1fc785b36c1e5a8a4
Mr.P_o_s_T <popovich601@gmail.com>
2018-06-08 18:56:27 +0300

    ObservableDouble - usual realisation of IObservable interface

---
+++ Ex.22/Ex.22/ObservableDouble.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "IObservable.h"
+#include <vector>
+
+class ObservableDouble : public IObservable<double> {
+public:
+	ObservableDouble() {}
+	virtual ~ObservableDouble() {}
+
+	void addObserver(IObserver<double> &o) {
+		observerList.push_back(o);
+	}
+
+	void removeObserver(IObserver<double> &o) {
+		for (auto item = observerList.begin(); item != observerList.end(); item++)
+			if (&*item == &o) {
+				observerList.erase(item);
+				break;
+			}
+	}
+
+	virtual void setValue(double newValue) {
+		value = newValue;
+		notify();
+	}
+protected:
+	std::vector<IObserver<double> &> observerList;
+
+	virtual void notify() {
+		for (auto item = observerList.begin(); item != observerList.end(); item++) (*item).updated(value);
+	}
+private:
+	double value;
+};
