c005164bd7880e263a3945c929e6c520f02857ae
Mr.P_o_s_T <popovich601@gmail.com>
2018-06-08 19:22:48 +0300

    Unit tests are added

---
+++ Ex.22/Ex.22_Unit-tests/ObserverUnitTest.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Ex22_Unittests {		
+	TEST_CLASS(ObserverUnitTests) {
+	public:
+		
+		TEST_METHOD(SummatorUnitTest) {
+			// TODO: Разместите здесь код своего теста
+		}
+
+	};
+}
