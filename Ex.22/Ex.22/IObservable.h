81810dad737347a14923a4a2be94c646214369bd
Mr.P_o_s_T <popovich601@gmail.com>
2018-06-08 18:06:54 +0300

    IObservable interface has been added

---
+++ Ex.22/Ex.22/IObservable.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "IObserver.h"
+
+template<class T>
+class IObservable {
+public:
+	IObservable() {}
+	virtual ~IObservable() {}
+
+	virtual void addObserver(IObserver &o) = 0;
+	virtual void removeObserver(IObserver &o) = 0;
+	virtual void notify() = 0;
+};
